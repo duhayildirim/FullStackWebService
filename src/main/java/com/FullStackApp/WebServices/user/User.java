795942d01bd3e9bea6afa37c4a85458d110a7a6d
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-03-02 15:30:21 +0300

    User object

---
+++ src/main/java/com/FullStackApp/WebServices/user/User.java
@@ -0,0 +1,49 @@
+package com.FullStackApp.WebServices.user;
+
+public class User {
+	private String userName;
+	private String displayName;
+	private String email;
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", displayName=" + displayName + ", email=" + email + ", password="
+				+ password + "]";
+	}
+	
+	
+
+}
--- src/main/java/com/FullStackApp/WebServices/user/UserController.java
+++ src/main/java/com/FullStackApp/WebServices/user/UserController.java
@@ -1,5 +1,26 @@
 package com.FullStackApp.WebServices.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// Kullanıcı ile ilgili olacak bütün http requestlerinin ulaştığı class olacak
+// bu sebeple
+// @RestController notionını ekledim bu sayede spring bir takım ayarlamalar
+// yapacak
 public class UserController {
-	
+    // Restful web servis yazıyorum
+    // Logluyorum. Belki belirli timestamplerde filelara yazabilirim bu logları.
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    // bu fonksiyonun bir post requestlerinde çalışmasını istiyorum.@PostMapping 'in
+    // içerisine bizim dinlediğimiz
+    // url pathini endpointmi veriyorum.
+    @PostMapping("/api/1.0/users")
+    public void createUser(@RequestBody User user) {
+        log.info(user.toString());
+    }
 }
